0511031c158a0f94b591708e89f43e1ef17c8ba1
ClementDeguise <cdeg09@gmail.com>
2018-10-10 11:41:11 +0200

    second commit

---
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -0,0 +1,29 @@
+package com.emse.spring.faircorp.hello;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class DummyUserService implements UserService {
+
+    @Autowired public GreetingService greeting;
+
+
+
+    public void greetAll(){
+        String A = "Elodie";
+        String B = "Charles";
+        greeting.greet(A);
+        greeting.greet(B);
+
+
+    }
+
+
+
+
+}
